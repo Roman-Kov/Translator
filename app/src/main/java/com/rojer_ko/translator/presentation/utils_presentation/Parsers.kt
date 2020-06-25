a91450280c6d6c958abf768352a40bb547390f7c
Roman-Kov <rojer_ko@bk.ru>
2020-06-25 19:35:03 +0300

    add Kotlin DSL and modules (s_ch)

--- app/src/main/java/com/rojer_ko/translator/presentation/utils_presentation/Parsers.kt
+++ app/src/main/java/com/rojer_ko/translator/presentation/utils_presentation/Parsers.kt
@@ -1,4 +1,4 @@
-package com.rojer_ko.translator.utils
+package com.rojer_ko.utils
 
 import com.rojer_ko.translator.data.datasource.room.HistoryEntity
 import com.rojer_ko.translator.data.model.AppState
